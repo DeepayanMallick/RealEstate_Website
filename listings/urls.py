87f0565558be80b2bd58e19a7cd0da7e24f7ab31
DeepayanMallick <deepayan1@live.com>
2018-12-08 17:04:45 +0600

    adding search files properly

--- listings/urls.py
+++ listings/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.listings, name='listings'),
     path('<int:listing_id>', views.listing, name='listing'),
-    path('search/', views.search, name='search'),
+    path('search', views.search, name='search'),
 ]
